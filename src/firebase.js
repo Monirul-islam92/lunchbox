af653c119078ac1e075ac58e6b68de00869a3e9b
Instapharm <md@omnee.ca>
2021-01-16 20:11:19 -0600

    second-commit

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+// This import loads the firebase namespace along with all its type information.
+import firebase from 'firebase/app';
+
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth';
+import 'firebase/database';
+
